9afdba06c4975adbd7ffc8bbc68f1940e6052539
Chuck Francis <bnsf5622@gmail.com>
2020-01-17 09:29:52 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TReturnToSender1._2.sln
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aMystery = Mystery();
+            string totallyUnexpected = Unexpected();
+            string aSurprise = Surprise();
+            string itsClassified = Classified();
+            string aSecret = Secret();
+
+            Console.WriteLine("The methods have returned! Their results...\n");
+            Console.WriteLine("Mysterious: " + aMystery);
+            Console.WriteLine(" Secret: " + aSecret);
+            Console.WriteLine("Suprising: " + aSuprise);
+            Console.WriteLine("Classified: " + itsClassified);
+            Console.WriteLine("Unexpected: " + totallyUnexpected);
+
+        }
+
+        public static int Secret()
+        {
+            return 42;
+        }
+
+        public static double Surprise()
+        {
+            return 3.14;
+        }
+
+        public static char Mystery()
+        {
+            return 'X';
+        }
+
+        public static bool Classified()
+        {
+            return true;
+        }
+
+        public static String Unexpected()
+        {
+            return "Spanish Inquisition";
+        }
+        
+    }
+}
